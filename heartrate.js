dadcab8d005479bb0e1b55810f03540fad0f3fa2
Corban Brook <corbanbrook@gmail.com>
2011-02-11 11:33:10 -0500

    Updated README usage documentation Added missing param for canvas context Fixed example

--- heartrate.js
+++ heartrate.js
@@ -1,4 +1,5 @@
 function HeartRate(options) {
+  this.context          = options && options.context || false;
   this.maxFPS           = options && options.maxFPS || 50;
   this.path             = options && options.path || "./heartrate";
   this.frameRate        = 0;
@@ -6,23 +7,26 @@ function HeartRate(options) {
   this.framesPerSecond  = 0;
   this.timeline         = new Array(16);
   this.lastUpdate       = (new Date()).getTime() / 1000;
-  
-  // Background template
-  this.bgImage = new Image(), this.bgImage.src = this.path + "/images/bg.png";
-  
-  this.hearts = [];
-  
-  for (var i = 0; i < 31; i ++) {
-    var prefix = i < 10 ? "spin000" : "spin00"; 
-    this.hearts.push(new Image());
-    this.hearts[i].src = this.path + "/images/" + prefix + i + ".png";
+  this.updated          = false;
+
+  if (this.context) {
+    // Background template
+    this.bgImage = new Image(), this.bgImage.src = this.path + "/images/bg.png";
+    
+    this.hearts = [];
+    
+    for (var i = 0; i < 31; i ++) {
+      var prefix = i < 10 ? "spin000" : "spin00"; 
+      this.hearts.push(new Image());
+      this.hearts[i].src = this.path + "/images/" + prefix + i + ".png";
+    }
+    
+    // Text color
+    this.textColor = "rgba(100, 64, 64, 1)";
+    
+    // Graph color
+    this.graphColor = "rgba(200, 64, 80, 1)";
   }
-  
-  // Text color
-  this.textColor = "rgba(100, 64, 64, 1)";
-  
-  // Graph color
-  this.graphColor = "rgba(200, 64, 80, 1)";
 }
 
 HeartRate.prototype.update = function() {
@@ -41,30 +45,39 @@ HeartRate.prototype.update = function() {
   }
   
   this.frameCount++;
+  this.updated = true;
 };
 
-HeartRate.prototype.render = function(x, y) {
-  var scaleX = 4;
+// This method calculates FPS and Renders the monitor
+HeartRate.prototype.monitor = function(x, y) {
+  // only update if update has not already been called somewhere else
+  !this.updated && this.update(), this.updated = false;
   
-  ctx.save();
-    ctx.translate(x, y);
+  var ctx = this.context;
+
+  if (ctx) {
+    var scaleX = 4;
     
-    // Background
-    ctx.drawImage(this.bgImage, 0, 0);
-    ctx.drawImage(this.hearts[frameCount % 30], 9, 6, 20, 18);
+    ctx.save();
+      ctx.translate(x, y);
+      
+      // Background
+      ctx.drawImage(this.bgImage, 0, 0);
+      ctx.drawImage(this.hearts[this.frameCount % 30], 9, 6, 20, 18);
+      
+      // Line graph
+      ctx.strokeStyle = this.graphColor;
+      
+      for (var i = 0, len = this.timeline.length - 1; i < len; i++) {
+        var plot1 = 25 - this.timeline[i] / this.maxFPS * 20, 
+            plot2 = 25 - this.timeline[i+1] / this.maxFPS * 20;
+                        
+        isNaN(plot1) || line(i * scaleX + 64, plot1, (i+1) * scaleX + 64, plot2);
+      }
     
-    // Line graph
-    ctx.strokeStyle = this.graphColor;
-    
-    for (var i = 0, len = this.timeline.length - 1; i < len; i++) {
-      var plot1 = 25 - this.timeline[i] / this.maxFPS * 20, 
-          plot2 = 25 - this.timeline[i+1] / this.maxFPS * 20;
-                      
-      isNaN(plot1) || line(i * scaleX + 64, plot1, (i+1) * scaleX + 64, plot2);
-    }
-  
-    // Text
-    ctx.fillStyle = this.textColor;
-    ctx.fillText(this.frameRate.toFixed(0), 33, 17);
-  ctx.restore();
+      // Text
+      ctx.fillStyle = this.textColor;
+      ctx.fillText(this.frameRate.toFixed(0), 33, 17);
+    ctx.restore();
+  }
 };
